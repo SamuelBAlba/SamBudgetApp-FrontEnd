dabedeff3efe1a78087e0686e9ab5078c904aef1
SamuelBAlba <brayanbalba24@gmail.com>
2023-04-22 23:29:28 -0400

    new form

--- src/Components/TransactionNewForm.js
+++ src/Components/TransactionNewForm.js
@@ -1,6 +1,23 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+const API = process.env.APP_API_URL;
+
 export default function TransactionsNewForm() {
+    const [transaction, setTransactions] = useState({
+        itemName: "",
+        amount: "",
+        date: "",
+        from: "",
+        category: ""
+    })
+
+
     return (
         <div>
+            <form>
+                <label></label>
+            </form>
 
         </div>
     )
